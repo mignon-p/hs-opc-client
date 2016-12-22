13a034bb6dc809af1a76093770cfe76ac234f9dc
Patrick Pelletier <code@funwithsoftware.org>
2016-12-22 12:24:40 -0800

    accept color as a command line argument

--- Opc.hs
+++ Opc.hs
@@ -7,6 +7,8 @@ import qualified Data.ByteString.Lazy as L
 import Data.Word
 import Network.Socket
 import Network.Socket.ByteString
+import System.Environment
+import System.Exit
 
 data Frame = SetColors
              { fChannel :: !Word8
@@ -65,17 +67,53 @@ instance Binary Pixel where
 
   get = Pixel <$> getWord8 <*> getWord8 <*> getWord8
 
-frame :: Frame
-frame = SetColors 0 $ replicate 50 $ Pixel 255 255 255
+nLights = 512                   -- max number of lights on a FadeCandy
+
+mkFrame :: Pixel -> Frame
+mkFrame c = SetColors 0 $ replicate nLights c
+
+black = Pixel 0 0 0
+blue = Pixel 0 0 255
+cyan = Pixel 0 255 255
+green = Pixel 0 255 0
+magenta = Pixel 255 0 255
+orange = Pixel 255 128 0
+purple = Pixel 192 0 255
+red = Pixel 255 0 0
+white = Pixel 255 255 255
+yellow = Pixel 255 255 0
+
+sendFrame :: Socket -> Frame -> IO ()
+sendFrame s f = sendMany s $ L.toChunks $ encode f
 
 main = do
+  args <- getArgs
+  color <- case args of
+             []          -> return white
+             ["black"]   -> return black
+             ["blue"]    -> return blue
+             ["cyan"]    -> return cyan
+             ["green"]   -> return green
+             ["magenta"] -> return magenta
+             ["orange"]  -> return orange
+             ["purple"]  -> return purple
+             ["red"]     -> return red
+             ["white"]   -> return white
+             ["yellow"]  -> return yellow
+             [r, g, b]   -> return $ Pixel (read r) (read g) (read b)
+             _ -> do
+               putStrLn "Argument must be one of:"
+               putStrLn "    black, blue, cyan, green, magenta, orange, purple, red, white, yellow"
+               putStrLn "Or three integer arguments between 0-255"
+               exitFailure
   ai <- getAddrInfo Nothing (Just "127.0.0.1" {- "localhost" -}) (Just "7890")
   let addr = addrAddress $ head ai
       fam (SockAddrInet {}) = AF_INET
       fam (SockAddrInet6 {}) = AF_INET6
       fam sa = error $ "Unexpected socket family: " ++ show sa
   s <- socket (fam addr) Stream defaultProtocol
-  -- putStrLn $ show addr
   connect s addr
-  let bs = encode frame
-  sendMany s (L.toChunks bs)
+  let f = mkFrame color
+  sendFrame s f
+  -- send frame twice to defeat FadeCandy's fade logic; we want immediate results!
+  sendFrame s f
